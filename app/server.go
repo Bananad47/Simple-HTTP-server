3208d9b61ec354bfe127268cd60ebc5e7f40a7af
Bananad47 <demid.shumakher@gmail.com>
2024-03-13 00:00:59 +0500

    I do it... AHHAHAH

--- app/server.go
+++ app/server.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"github.com/codecrafters-io/http-server-starter-go/internal/request"
+	"github.com/codecrafters-io/http-server-starter-go/internal/response"
 	"log"
 	"net"
+	"regexp"
+	"strings"
 )
+
+var headers = map[string]string{
+	"Content-Type": "text/plain",
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	//fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
+	fmt.Println("Server Started")
 	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		log.Fatalln("Failed to bind to port 4221")
@@ -20,11 +29,23 @@ func main() {
 	if err != nil {
 		log.Fatalln("Error accepting connection: ", err.Error())
 	}
+
+	var test strings.Builder
+
 	req, err := request.ParseRequest(connection)
-	if err != nil || req.Path != "/" {
-		connection.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	if err != nil {
+		resp := response.CreateResponse(req.Path, "404 Not Found", headers)
+		resp.WriteResponse(connection)
 	} else {
-		connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		resp := response.CreateResponse(req.Path, "200 OK", headers)
+		t, _ := regexp.Compile("/echo/([\\w]+)")
+		m := t.FindStringSubmatch(req.Path)
+		if len(m) != 0 {
+			resp.AddContent(m[1])
+		}
+		resp.WriteResponse(connection)
+		resp.WriteResponse(&test)
 	}
+	fmt.Println(test.String())
 	connection.Close()
 }
---
+++ internal/response/response.go
@@ -0,0 +1,45 @@
+package response
+
+import (
+	"fmt"
+	"io"
+	"strconv"
+)
+
+type Response struct {
+	Headers     map[string]string
+	Path        string
+	HttpVersion string
+	StatusCode  string
+	Content     string
+}
+
+func CreateResponse(path string, statuscode string, headers map[string]string) *Response {
+	resp := &Response{}
+	resp.Path = path
+	resp.HttpVersion = "HTTP/1.1"
+	resp.StatusCode = statuscode
+	resp.Headers = headers
+	resp.Headers["Content-Length"] = "0"
+	return resp
+}
+
+func (resp *Response) AddContent(content string) {
+	resp.Content = content
+	resp.Headers["Content-Length"] = strconv.Itoa(len([]byte(content))) + "\r\n"
+}
+
+func (resp *Response) WriteResponse(wr io.Writer) error {
+	_, err := fmt.Fprintf(wr, "%s %s\r\n", resp.HttpVersion, resp.StatusCode)
+	if err != nil {
+		return err
+	}
+	for key, val := range resp.Headers {
+		_, err = fmt.Fprintf(wr, "%s: %s\r\n", key, val)
+		if err != nil {
+			return err
+		}
+	}
+	_, err = fmt.Fprintf(wr, "%s\r\n\r\n", resp.Content)
+	return err
+}
